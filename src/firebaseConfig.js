f749a0d46bdc8f53de93f3223142888ff3a153ab
alwinajai <alwin6000@gmail.com>
2025-07-04 15:56:46 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhms9Sm6knxizFYRl19oTK1JPvdbRSRbw",
+  apiKey: "apikey",
   authDomain: "fileapp-5ee5d.firebaseapp.com",
   projectId: "fileapp-5ee5d",
   storageBucket: "fileapp-5ee5d.firebasestorage.app",
